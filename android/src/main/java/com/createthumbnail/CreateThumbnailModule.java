4ec882c2b501b6a94218ccc4c857e438ed231ce2
thanhlx <thanhlx1298@gmail.com>
2023-09-06 16:49:44 +0700

    Update try/catch for retriever.release

--- android/src/main/java/com/createthumbnail/CreateThumbnailModule.java
+++ android/src/main/java/com/createthumbnail/CreateThumbnailModule.java
@@ -189,7 +189,13 @@ public class CreateThumbnailModule extends ReactContextBaseJavaModule {
         }
   
         Bitmap image = retriever.getFrameAtTime(time * 1000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
-        retriever.release();
+
+        try {
+            retriever.release();
+        } catch (IOException e) {
+            // Do nothing. We can't handle this, and this is usually a system problem
+        }
+        
         if (image == null) {
             throw new IllegalStateException("File doesn't exist or not supported");
         }
